dea9dbab1704a7ec2e04d369536bac2032ec7699
mmoralesntech <157641657+mmoralesntech@users.noreply.github.com>
2024-01-24 18:13:42 -0600

    Update main.js time 1 minute

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ setInterval(function() {
   
   // Mostrar el nuevo iframe
   iframes[currentIframe].style.display = 'block';
-}, 45000);//Cada minuto
+}, 60000);//Cada minuto
 
 // // Función para actualizar el contenido de los iframes con AJAX cada 5 minutos
 // setInterval(function() {
